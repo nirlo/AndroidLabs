b63a54cb36e3c277c1f7194d9d313de849b79f48
nirlo <nrlockhart@gmail.com>
2018-04-09 14:26:11 -0400

    finished lab 7

---
+++ app/src/main/java/com/example/lock0134/androidlabs/MessageFragment.java
@@ -0,0 +1,10 @@
+package com.example.lock0134.androidlabs;
+
+import android.app.Fragment;
+
+/**
+ * Created by lock0134 on 2018-04-04.
+ */
+
+public class MessageFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/lock0134/androidlabs/WeatherForecast.java
@@ -0,0 +1,13 @@
+package com.example.lock0134.androidlabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeatherForecast extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_forecast);
+    }
+}
